2b3113d5945afbf1f9daa9273d6331ad4cb1682d
shaun5481 <shaunklekalakala@gmail.com>
2021-07-10 22:55:18 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Button from '@material-ui/core/Button';
 
+function ButtonStyle() {
+  const classes = useStyles();
+  return <Button className={classes.root}>shaun</Button>;
+}
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Curiculum Vitea of Shaun Lekalakala!</h1>
+      
+      <Button variant="contained" Size="Small" color="Secondary">
+        Hello World
+      </Button>
     </div>
   );
 }
