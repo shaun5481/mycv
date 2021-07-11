23a999b4931b772b712befe2b0ce768d887d9156
shaun5481 <shaunklekalakala@gmail.com>
2021-07-11 19:22:14 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -23,7 +23,12 @@ export default function FullWidthGrid() {
       <Grid container spacing={1}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h1">Shaun Lekalakala</Typography>
+            <Typography 
+            variant="h6"
+            component="h2"
+            gutterBottom
+            >
+              Shaun Lekalakala</Typography>
           </Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
