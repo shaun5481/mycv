24b59f3a75c456ade0065d6f34ea3c95462d4102
shaun5481 <shaunklekalakala@gmail.com>
2021-07-11 21:58:50 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles(theme => ({
   root: {
